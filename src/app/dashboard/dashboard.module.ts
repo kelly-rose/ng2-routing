5a05863387e9c7f360c94c97c2d30175a5ab906c
Kelly <kellyrose.evergreen@gmail.com>
2017-08-23 17:10:15 +0900

    19th : 20 - dashboard user details form (important!)

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,11 +6,13 @@ import { DashboardUsersComponent } from './users/dashboard-users.component';
 import { DashboardUsersHomeComponent } from './users/dashboard-users-home.component';
 import { DashboardUserDetailsComponent } from './users/dashboard-user-details.component';
 import {UserService} from "../shared/services/user.service";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
-    dashboardRouting
+    dashboardRouting,
+    FormsModule
   ],
   declarations: [
     DashboardComponent,
--- src/app/dashboard/users/dashboard-user-details.component.ts
+++ src/app/dashboard/users/dashboard-user-details.component.ts
@@ -6,15 +6,25 @@ import {User} from "../../shared/models/user";
 @Component({
   selector: 'app-dashboard-users-detail',
   template: `
-    <p *ngIf="user">
-      {{user.name}}
-    </p>
+    <div class="jumbotron">
+      <div *ngIf="user">
+        <h2>{{ user.name }}</h2>
+        <div class="form-group">
+          <input type="text" [(ngModel)]="editName" class="form-control">
+        </div>
+        <div class="form-group text-center">
+          <button (click)="cancel()" class="btn btn-danger">Cancel</button>
+          <button (click)="save()" class="btn btn-success">Save</button>
+        </div>
+      </div>
+    </div>
   `,
   styles: []
 })
 export class DashboardUserDetailsComponent implements OnInit {
 
   user: User;
+  editName: string;
 
   constructor(private router: Router, private route: ActivatedRoute, private service: UserService) {
   }
@@ -23,7 +33,17 @@ export class DashboardUserDetailsComponent implements OnInit {
     //see video 00:11:11~00:11:50
     this.route.params.forEach(param => {
       console.log(param);
-      this.service.getUser(param.username).then(user => this.user = user);
+      this.service.getUser(param.username).then(user => {
+        this.user     = user;
+        this.editName = user.name;
+      });
     });
+  }
+  cancel(){
+    this.router.navigate(['/dashboard/users']);
+  }
+  save(){
+    this.user.name = this.editName;
+    this.router.navigate(['/dashboard/users']);
   }
 }
